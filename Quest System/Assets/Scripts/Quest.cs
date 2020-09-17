4f7c9d9c70c44a54dfab4993ef44029ebb5d7fe7
Jing <jtan1993@gmail.com>
2020-09-17 08:44:08 +0100

    update quest manager

--- Quest System/Assets/Scripts/Quest.cs
+++ Quest System/Assets/Scripts/Quest.cs
@@ -2,28 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//main graph class for quest system
 public class Quest
 {
     public List<QuestEvent> questEvents = new List<QuestEvent>();
-    List<QuestEvent> pathList = new List<QuestEvent>();
+    //each event has a list of the paths that goes between one & the other: don't need another pathlist going on in this quest
+    List<QuestEvent> pathList = new List<QuestEvent>(); //quest events in pathlist are events in the path, not the paths themselves. 
 
     public Quest()
     { }
 
-    public QuestEvent AddQuestEvent(string n, string d)
+    //main method for creating an event and add it to the Quest at the same time: pass through name & description, used by QuestManager
+    public QuestEvent AddQuestEvent(string evtName, string evtDescription)
     {
-        QuestEvent questEvent = new QuestEvent(n, d);
+        QuestEvent questEvent = new QuestEvent(evtName, evtDescription);
         questEvents.Add(questEvent);
         return questEvent;
     }
 
+    //construct a path between fromQuestEvent & toQuestEvent ids: allow quests with the same names
+    //before adding any paths: must add all events to serve those paths to prevent errors
     public void AddPath(string fromQuestEvent, string toQuestEvent)
     {
-        questEvent from = findQuestEvent(fromQuestEvent),
+        QuestEvent from = FindQuestEvent(fromQuestEvent);
+        QuestEvent to = FindQuestEvent(toQuestEvent);
+        if (from != null && to != null)
+        {
+            QuestPath path = new QuestPath(from, to);
+            from.pathList.Add(path);
+        }
     }
 
-    QuestEvent findQuestEvent(string id)
+    //Loop through all the quests it got hold off & find the correct one using id
+    QuestEvent FindQuestEvent(string id)
     {
+        foreach (QuestEvent evt in questEvents)
+        {
+            if (evt.GetID() == id)
+            {
+                return evt;
+            }
+        }
+        return null;
+    }
 
+    //debug what you put in the graph afterwards: loop through all quest events & print their given name
+    public void PrintPath()
+    {
+        foreach (QuestEvent evt in questEvents)
+        {
+            Debug.Log(evt.eventName);
+        }
     }
 }
--- Quest System/Assets/Scripts/QuestManager.cs
+++ Quest System/Assets/Scripts/QuestManager.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestManager : MonoBehaviour
 {
+    public Quest quest = new Quest();
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        QuestEvent a = quest.AddQuestEvent("test1", "description1");
+        QuestEvent b = quest.AddQuestEvent("test2", "description2");
+        QuestEvent cOpt = quest.AddQuestEvent("test3", "description3"); //optional quest
+        QuestEvent dOpt = quest.AddQuestEvent("test4", "description4"); //optional quest
+        QuestEvent e = quest.AddQuestEvent("test5", "description5");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //define the paths between events: the order they must be completed
+        quest.AddPath(a.GetID(), b.GetID());
+        quest.AddPath(b.GetID(), cOpt.GetID()); //choosen 1 of the optional paths
+        quest.AddPath(b.GetID(), dOpt.GetID()); //choosen 1 of the optional paths
+        quest.AddPath(cOpt.GetID(), e.GetID()); //optional chosen path variant to end quest
+        quest.AddPath(dOpt.GetID(), e.GetID()); //optional chosen path variant to end quest
+
+        quest.PrintPath(); //check all paths have been added into quest
     }
 }
